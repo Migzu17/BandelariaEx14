8338bf72ce04506da11cd270b793bfe69d30f30c
222143 <222143@LT24-L1-1154.sbcdom.com>
2024-10-17 10:04:18 +0800

    Added test cases back to repo

---
+++ bandelariaex14-maven/src/test/java/org/acumen/training/codes/test/TestTrapArray.java
@@ -0,0 +1,12 @@
+package org.acumen.training.codes.test;
+
+import org.acumen.training.codes.TrapArray;
+import org.junit.jupiter.api.Test;
+
+public class TestTrapArray {
+	@Test
+	public void testArrayTransact() {
+		TrapArray trap = new TrapArray();
+		trap.arrayTransact();
+	}
+}
